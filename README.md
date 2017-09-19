db34231372e1f964a5120e110564f6a2fe892599
Eric Alcaide <ericalcaide1@gmail.com>
2017-09-19 22:12:06 +0200

    update readme and hashed ledger concept

--- README.md
+++ README.md
@@ -4,15 +4,33 @@ A Python Implementation of a simple blockchain. Less than 150 lines of code.
 
 ![Build Status][build-image]
 
-Basic methods:
-* **Add Block:** Allows to add a block to the chain and store some data in it.
-* **Show Genesis Block:** Shows the genesis block attributes.
-* **Show Last Block:** Shows the last block attributes.
-* **Show n Block:** Shows the Nth block attributes.
-* **Show Chain:** Shows the whole chain, one block at a time.
-* **Shutdown:** Terminate the program and delete the blockchain.
+## Description
 
-### "A Blockchain is a linked list data structure where the links are hash pointers." - Alex Sicart
+The Pysimplechain implementation is focused almost exclusively in the hashed ledger feature. It does not include any advanced feature like a distributed ledger or a consensus protocol via proof of work. Here you'll also find that the idea of the "transaction" is abstracted to a more general concept of "message" that can contain any type of data.
+
+For that reason, the goal of this project is to explain and to make clearer how is a blockchain structured at the very core. It's not built with the intention to replicate an advanced blockchain like Bitcoin or Ethereum.
+
+The following blockchain implemented in the simple_chain.py file is composed of 3 classes. The `Message()` class, the `Block()` class and the `Chain()`.
+
+A `message` is the basic data container. It is sealed when added to a block and has 2 hashes that identify it: the payload hash and the block hash.
+Each message is linked to the previous message via hash pointers (the `prev_hash` attribute). The `validate` message method will ensure the integrity of each message, but will not check if the hash pointers are correct. This is left to the `validate` method in the `Block()` class.
+
+A `block` can contain 1,...,n messages that are linked sequentially one after the other. When a `block` is added to the `chain`, it's sealed and validated to
+ensure that the messages are correctly ordered and the hash pointers match. Once the block is sealed and hashed, it is validated by checking the expected vs the actual.
+
+A `chain` can contain 1,...,m blocks that are linked sequentially one after another. The chain integrity can be validated at any time calling the `validate` method, which will call each block's validate method and will raise an `InvalidBlockchain` exception.
+
+## Interactivity:
+
+A `manager()` function is provided to interact with the blockchain via the Terminal/Console. The basic actions are:
+
+* **Add Message to Block:** Allows to add a message to the current block.
+* **Add Block to Chain:** Allows to add the current block to the chain if it's not empty.
+* **Show Block:** Asks for an index and if exists a block with that index, returns some of the block attributes.
+* **Show Chain:** Returns some of the block attributes for each block in the chain.
+* **Validate Integrity:** Returns True if the integrity is validated, terminates the program raisiing the appropiate exception otherwise.
+* **Exit:** Terminates the program and deletes the blockchain.
+
 
 ## Contribute
 Hey there! New ideas are welcome: open/close issues, fork the repo and share your code with a Pull Request.
--- simple_chain.py
+++ simple_chain.py
@@ -5,143 +5,186 @@
 	to the previous block. It's a basic implementation
 	and does NOT include advanced features like
 	prove of work or peer to peer.
-
 """
 
-import hashlib
 import datetime
+import hashlib
+import time
 
-class Chain():
+class Message():
+	def __init__(self, data):
+		self.hash = None
+		self.prev_hash = None
+		self.timestamp = time.time()
+		self.size = len(data.encode('utf-8'))   # length in bytes
+		self.data = data
+		self.payload_hash = self._hash_payload()
+
+	def _hash_payload(self):
+		return hashlib.sha256(bytearray(str(self.timestamp) + str(self.data), "utf-8")).hexdigest()
+
+	def _hash_message(self):
+		return hashlib.sha256(bytearray(str(self.prev_hash) + self.payload_hash, "utf-8")).hexdigest()
+
+	def link(self, message):
+		""" Link the message to the previous one via hashes."""
+		self.prev_hash = message.hash
+		# return self
+
+	def seal(self):
+		""" Get the message hash. """
+		self.hash = self._hash_message()
+		# return self
+
+	def validate(self):
+		""" Check whether the message is valid or not. """
+		if self.payload_hash != self._hash_payload():
+			raise InvalidMessage("Invalid payload hash in message: " + str(self))
+		if self.hash != self._hash_message():
+			raise InvalidMessage("Invalid message hash in message: " + str(self))
+
+	def __repr__(self):
+		return 'Message<hash: {}, prev_hash: {}, data: {}>'.format(
+			self.hash, self.prev_hash, self.data[:20]
+		)
+
+
+class Block:
+	def __init__(self, *args):
+		self.messages = []
+		self.timestamp = None
+		self.prev_hash = None
+		self.hash = None
+		if args:
+			for arg in args:
+				self.add_message(arg)
+
+	def _hash_block(self):
+		return hashlib.sha256(bytearray(str(self.prev_hash) + str(self.timestamp) + self.messages[-1].hash, "utf-8")).hexdigest()
+
+	def add_message(self, message):
+		if len(self.messages) > 0:
+			message.link(self.messages[-1])
+		message.seal()
+		message.validate()
+		self.messages.append(message)
+ 
+	def link(self, block):
+		""" The block hash only incorporates the head message hash
+			which then transitively includes all prior hashes.
+		"""
+		self.prev_hash = block.hash
+        
+	def seal(self):
+		self.timestamp = time.time()
+		self.hash = self._hash_block()
+
+	def validate(self):
+		""" Validates each message hash, then chain integrity, then the block hash.
+			Calls each message's validate() method.
+
+			If a message fails validation, this method captures the exception and 
+			throws InvalidBlock since an invalid message invalidates the whole block.
+		"""
+		for i, msg in enumerate(self.messages):
+			try:
+				msg.validate()
+				if i > 0 and msg.prev_hash != self.messages[i-1].hash:
+					raise InvalidBlock("Invalid block: Message #{} has invalid message link in block: {}".format(i, str(self)))
+			except InvalidMessage as ex:
+				raise InvalidBlock("Invalid block: Message #{} failed validation: {}. In block: {}".format(
+					i, str(ex), str(self))
+				)
+
+	def __repr__(self):
+		return 'Block<hash: {}, prev_hash: {}, messages: {}, time: {}>'.format(
+			self.hash, self.prev_hash, len(self.messages), self.timestamp
+		)
+
+class SimpleChain:
 	def __init__(self):
 		self.chain = []
 
-	# Create the first block
-	def genesis(self):
-		index = 0
-		timestamp = datetime.datetime.now()
-		data = "I'm the first Block! Feels good to start a Blockchain."
-		previous_hash = " "
-
-		self.chain.append(Block(index, timestamp, data, previous_hash))
-		return
-
-	# Add a block if valid
 	def add_block(self, block):
-		if self.check_block(block) == True:
-			self.chain.append(block)
-			print()
-			print("Block #"+str(block.index)+" added to the blockchain")
-			print("Hash: ", block.hash)
+		""" Add a block if valid."""
+		if len(self.chain) > 0:
+			block.prev_hash = self.chain[-1].hash
+		block.seal()
+		block.validate()
+		self.chain.append(block)
+
+	def validate(self):
+		""" Validates each block, in order.
+			An invalid block invalidates the chain.
+		"""
+		for i, block in enumerate(self.chain):
+			try:
+				block.validate()
+			except InvalidBlock as exc:
+				raise InvalidBlockchain("Invalid blockchain at block number {} caused by: {}".format(i, str(exc)))
+		return True
+
+	def __repr__(self):
+		return 'SimpleChain<blocks: {}>'.format(len(self.chain))
+
+
+class InvalidMessage(Exception):
+	def __init__(self,*args,**kwargs):
+		Exception.__init__(self,*args,**kwargs)
+
+class InvalidBlock(Exception):
+	def __init__(self,*args,**kwargs):
+		Exception.__init__(self,*args,**kwargs)
+
+class InvalidBlockchain(Exception):
+	def __init__(self,*args,**kwargs):
+		Exception.__init__(self,*args,**kwargs)
+
+
+def manager():
+	chain = SimpleChain()
+	block = Block()
+	msg = """
+		Basic implementation of a Blockchain. Changes are inmutable. Be aware.
+
+		Action set:
+			- add message to the existing block  (1)
+			- add existing block to the chain    (2)
+			- show a block (index will be asked) (3)
+			- show the whole chain               (4)
+			- validate the chain integrity       (5)
+			- exit the program                   (6)
+
+		The validate action will kill the program if the integrity f the chain
+		is compromised.
+		"""
+
+	print(msg)	
+	while True:
+		print()
+
+		decide = input("Your action: ")
+
+		if decide == "1":
+			block.add_message(Message(input("Enter your data:")))
+		elif decide == "2":
+			if len(block.messages) > 0:
+				chain.add_block(block)
+				block = Block()
+			else: print("Block is empty, try adding some messages")
+		elif decide == "3":
+			index = int(input("Provide the index: "))
+			if len(chain.chain)>0:
+				try: print(chain.chain[index])
+				except: print("An issue occurred")
+		elif decide == "4":
+			for b in chain.chain:
+				print(b)
+				print("----------------")
+		elif decide == "5":
+			if chain.validate(): print("Integrity validated.")
 		else:
-			print("Block not added. Invalid block so far.")
-
-	def new(self):
-		prev = self.chain[-1]
-
-		index = prev.index+1
-		timestamp = datetime.datetime.now()
-		data = str(input("Data to be stored: "))
-		previous_hash = prev.hash
-
-		return Block(index, timestamp, data, previous_hash)
-
-	# check if the block recieved is valid
-	def check_block(self,block):
-		prev = self.chain[-1]
-		if block.previous_hash == prev.hash and block.index == prev.index+1:
-			return True
-		else:
-			return False
-
-	# Print block
-	def show_block(self, block):
-		print("--------------------------")
-		print("Showing Block")
-		print("Index: ", block.index)
-		# print("Timestamp: ", block.timestamp)
-		print("Data: ", block.data)
-		print("Hash: ", block.hash)
-		print("Previous hash: ", block.previous_hash)
-
-	# Show a node provided its index
-	def show_n_block(self, index):
-		try: 
-			block = self.chain[index]
-			return self.show_block(block)
-		except: 
-			print("Index out of range")
-
-	# Show latest block
-	def last_block(self):
-		last = self.chain[-1]
-		return self.show_block(last)
-
-	# Show latest block
-	def first_block(self):
-		first = self.chain[0]
-		return self.show_block(first)
-
-	# Show the entire chain
-	def show_chain(self):
-		print("/////////////////////////////////")
-		print("SHOWING CHAIN")
-		for block in self.chain:
-			self.show_block(block)
-
-	def manager(self):
-		self.genesis()
-		print()
-		print("Basic implementation of a Blockchain. Changes are inmutable. Be aware.")
-		print()
-		print("Action set:")
-		print()
-		print("- add_block (1)")
-		print("- show_block (2)")
-		print("- last_block (3)")
-		print("- first_block (4)")
-		print("- show_chain (5)")
-		print("- shutdown (6)")
-		while True:
-			print()
-
-			decide = str(input("Your action: "))
-
-			if decide == "1":
-				self.add_block(self.new())
-			elif decide == "2":
-				index = int(input("Block Index: "))
-				self.show_n_block(index)
-			elif decide == "3":
-				self.last_block()
-			elif decide == "4":
-				self.first_block()
-			elif decide == "5":
-				self.show_chain()
-			elif decide == "6":
-				break
-			else:
-				print("Invalid action")
-
-class Block():
-	def __init__(self, index, timestamp, data, previous_hash):
-		self.index = index
-		self.timestamp = timestamp
-		self.data = data
-		self.previous_hash = previous_hash
-		self.hash = self.encrypt_block()
-
-	# Returns the hash of the block
-	def encrypt_block(self):
-		sha = hashlib.sha256()
-
-		sha.update(str(self.index).encode('utf-8') +
-			str(self.timestamp).encode('utf-8') +
-			str(self.data).encode('utf-8') +
-			str(self.previous_hash).encode('utf-8'))
-
-		return sha.hexdigest()
-
+			break
 
 if __name__ == "__main__":
-	chain = Chain()
-	chain.manager()
+	manager()
